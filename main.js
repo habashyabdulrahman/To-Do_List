3dcb5052523a478108962847b7a80498762ab1d0
Habashy Abdulrahman <133090767+habashyabdulrahman@users.noreply.github.com>
2023-07-19 21:58:31 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("inbut-box");
+const listContent = document.getElementById("list-content");
+
+function addTask() {
+    if(inputBox.value === ""){
+        alert('Please enter a task');
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContent.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContent.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContent.innerHTML);
+}
+
+function showTask(){
+    listContent.innerHTML = localStorage.getItem("data");
+}
+showTask();
